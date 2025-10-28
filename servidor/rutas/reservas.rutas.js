1ed0874f26490322b54ba0ad18f54f408fbec8f3
HiFerno <fernolopez123@gmail.com>
2025-10-28 13:54:37 -0600

    actualizacion de rutas, modificar y cancelar reserva

--- servidor/rutas/reservas.rutas.js
+++ servidor/rutas/reservas.rutas.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
-const { crearReserva } = require('../controladores/reservas.controlador');
+const { crearReserva,
+        modificarReserva,
+        cancelarReserva
+    } = require('../controladores/reservas.controlador');
 const validarJWT = require('../middleware/validarJWT'); // <-- Importamos el middleware
 
 const router = Router();
@@ -9,5 +12,11 @@ const router = Router();
 // 2. Si el token es válido, pasa a crearReserva.
 // 3. Si no es válido, validarJWT devuelve un error 401.
 router.post('/', [validarJWT], crearReserva);
+// [PATCH] Modificar una reserva (Ruta nueva)
+// Usamos PATCH porque es una actualización parcial (solo cambia el asiento_id)
+router.patch('/', [validarJWT], modificarReserva);
+
+// [DELETE] Cancelar una reserva (Ruta nueva)
+router.delete('/', [validarJWT], cancelarReserva);
 
 module.exports = router;
