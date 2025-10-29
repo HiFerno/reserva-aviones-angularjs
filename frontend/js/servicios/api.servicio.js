5ec7f074da9da764fa854d227f775743ebd9e29e
HiFerno <fernolopez123@gmail.com>
2025-10-28 19:42:47 -0600

    correcion comentarios

--- frontend/js/servicios/api.servicio.js
+++ frontend/js/servicios/api.servicio.js
@@ -1,57 +1,58 @@
+// ACA CREO EL MODULO DE LA APP Y LE METO LA FABRICA DE SERVICIOS API
 angular.module('appVuelos')
     .factory('ApiServicio', function($http, API_URL) {
         
+        // CREO UN OBJETO VACIO PARA METER TODAS LAS FUNCIONES DE LA API
         const apiFactory = {};
 
-        // --- Asientos ---
+        // ESTA FUNCION ME TRAE TODOS LOS ASIENTOS DISPONIBLES
+        // EL INTERCEPTOR YA ME METE EL TOKEN ASI QUE NI ME PREOCUPO
         apiFactory.obtenerAsientos = function() {
-            // (Gracias al interceptor, esto ya lleva el token)
             return $http.get(API_URL + '/asientos');
         };
 
-        // --- Reservas ---
+        // ESTA ES PARA CREAR UNA RESERVA NUEVA CON LOS DATOS QUE LE MANDE
         apiFactory.crearReserva = function(datosReserva) {
             return $http.post(API_URL + '/reservas', datosReserva);
         };
 
+        // ACA MODIFICO UNA RESERVA QUE YA EXISTE
         apiFactory.modificarReserva = function(datosModificacion) {
-            // (PATCH /api/reservas)
             return $http.patch(API_URL + '/reservas', datosModificacion);
         };
 
-        // --- AÑADIR ESTA FUNCIÓN ---
+        // ESTA ES PARA CANCELAR UNA RESERVA
+        // EL DELETE ES MEDIO RARO PORQUE LOS DATOS VAN EN LOS PARAMS
         apiFactory.cancelarReserva = function(datosCancelacion) {
-            // (DELETE /api/reservas)
-            // $http.delete es especial, los datos van en un objeto 'config'
             return $http.delete(API_URL + '/reservas', { params: datosCancelacion });
         };
 
-        // --- AÑADIR: REPORTES ---
+        // ESTA ME TRAE TODOS LOS REPORTES QUE HAYA
         apiFactory.obtenerReportes = function() {
             return $http.get(API_URL + '/reportes');
         };
 
-        // --- AÑADIR: DESCARGAR XML ---
+        // ACA BAJO EL XML CON TODAS LAS RESERVAS
+        // LE DIGO QUE ME VA A DEVOLVER UN ARCHIVO (BLOB) Y NO JSON
         apiFactory.descargarXML = function() {
-            // Le pedimos a Angular que espere una respuesta tipo 'blob' (un archivo)
-            // en lugar de JSON.
             return $http.get(API_URL + '/archivos/descargar-xml', {
                 responseType: 'blob' 
             });
         };
 
-        // --- AÑADIR: CARGAR XML ---
+        // ESTA ES PARA SUBIR UN ARCHIVO XML
+        // USO FORMDATA PORQUE ES LA FORMA CORRECTA DE MANDAR ARCHIVOS
         apiFactory.cargarXML = function(archivo) {
-            // FormData es la forma estándar de enviar archivos en HTTP
             const fd = new FormData();
-            fd.append('archivo', archivo); // 'archivo' debe coincidir con el backend
+            fd.append('archivo', archivo); // EL NOMBRE 'archivo' TIENE QUE SER IGUAL QUE EN EL BACKEND
 
+            // ESTOS SON UNOS TRUQUITOS PARA QUE ANGULAR NO SE VUELVA LOCO CON EL FORMDATA
             return $http.post(API_URL + '/archivos/cargar-xml', fd, {
-                // Trucos para que Angular maneje FormData correctamente:
                 transformRequest: angular.identity,
                 headers: { 'Content-Type': undefined }
             });
         };
 
+        // DEVUELVO EL OBJETO CON TODAS LAS FUNCIONES
         return apiFactory;
     });
